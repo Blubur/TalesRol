ec4e2af24f14869260a1e6a23ad2e1ce8860fde5
Blubur <pililahiguera@gmail.com>
2026-05-18 23:48:56 +0200

    eliminar linea

--- src/app/(main)/admin/AdminRoomsTable.tsx
+++ src/app/(main)/admin/AdminRoomsTable.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { deleteRoom, changeRoomStatus } from '../actions'
 import {
   PencilSquareIcon,
   TrashIcon,
